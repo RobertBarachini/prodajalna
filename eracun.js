1153d48de6eee8dcbe86c485297739d5ad272241
Robert Barachini <robert.barachini@gmail.com>
2016-05-06 22:34:25 +0000

    Vmesni

--- eracun.js
+++ eracun.js
@@ -148,7 +148,7 @@ var strankaIzRacuna = function(racunId, callback) {
 
 // Izpis računa v HTML predstavitvi na podlagi podatkov iz baze
 streznik.post('/izpisiRacunBaza', function(zahteva, odgovor) {
-  odgovor.redirect('/izpisiRacun');
+  //odgovor.redirect('/izpisiRacun');
 })
 
 // Izpis računa v HTML predstavitvi ali izvorni XML obliki
